e024bef4c0f51988e29410080180f87de61b617a
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-01-26 19:35:40 +0400

    commit bifore merge

--- src/qaFactory.js
+++ src/qaFactory.js
@@ -25,10 +25,72 @@ function questionsAndAnswers(question, icon, answer) {
 
     if (!answerOpen) {
       // Answer is closed, create and append it
-      let questionAnswer = document.createElement("p");
-      questionAnswer.classList.add("answer-paragraph");
-      questionAnswer.textContent = `${answer}`;
-      questionDiv.appendChild(questionAnswer);
+
+      if (answer !== "answer I") {
+        let questionAnswer = document.createElement("p");
+        questionAnswer.classList.add("answer-paragraph");
+        questionAnswer.textContent = `${answer}`;
+        questionDiv.appendChild(questionAnswer);
+      } else {
+        // adding firts long qestion paragraph
+        let questionAnswer = document.createElement("div");
+        questionAnswer.classList.add("answer-paragraph");
+        questionDiv.appendChild(questionAnswer);
+
+        let questionAnswerDiv = document.createElement("div");
+        questionAnswerDiv.classList.add("answer-paragraph-div");
+
+        let questionAnswertext = document.createElement("p");
+        questionAnswertext.textContent = `ურსზე რეგისტრაციისთვის უნდა გაიარო რამდენიმე ეტაპი:`;
+        questionAnswer.appendChild(questionAnswertext);
+        questionAnswer.appendChild(questionAnswerDiv);
+
+        function createAndAppend(tag, text, parent) {
+          const element = document.createElement(tag);
+          element.textContent = text;
+          if (parent) {
+            parent.appendChild(element);
+          }
+          return element;
+        }
+
+        const stage1 = createAndAppend("div", "", questionAnswerDiv);
+        createAndAppend("strong", "I ეტაპი", stage1);
+        createAndAppend(
+          "span",
+          " - პირველ ეტაპზე საჭიროა, შეავსო სასურველი კურსისთვის განკუთვნილი სარეგისტრაციო ფორმა, რომელიც განთავსებულია კურსის შიდა გვერდზე. კურსზე რეგისტრაციის დასრულების შემდეგ დაიწყება შემოსული განცხადებების გადარჩევა.",
+          stage1,
+        );
+
+        const stage2 = createAndAppend("div", "", questionAnswerDiv);
+        createAndAppend("strong", "II ეტაპი", stage2);
+        createAndAppend(
+          "span",
+          " - შერჩევის მეორე ეტაპი კურსების მიხედვით განსხვავებულია, ზოგიერთი კურსისთვის მოიცავს პრე-ტესტს, ზოგიერთ კურსზე კი უნარების ტესტს, სადაც მინიმალური ზღვარის გადალახვის შემთხვევაში გადახვალ შერჩევის შემდეგ ეტაპზე.",
+          stage2,
+        );
+
+        const stage3 = createAndAppend("div", "", questionAnswerDiv);
+        createAndAppend("strong", "III ეტაპი", stage3);
+        createAndAppend(
+          "span",
+          " - მესამე ეტაპი კურსების მიხედვით განსხვავდება: Advance კურსებზე, სადაც მოითხოვება გარკვეული ტექნიკური ცოდნა, მონაწილეებმა უნდა დაწერონ ტექნიკური ტესტი ცოდნის დონის შესამოწმებლად, ხოლო კურსებზე, სადაც რაიმე ტიპის წინასწარი ცოდნა მოთხოვნილი არ არის უნდა შეასრულოთ ტექნიკური დავალება, რაც",
+          stage3,
+        );
+
+        const stage4 = createAndAppend("div", "", questionAnswerDiv);
+        createAndAppend("strong", "IV ეტაპი", stage4);
+        createAndAppend(
+          "span",
+          " - შერჩევის ბოლო მეოთხე ეტაპი მოიცავს გასაუბრებას შესარჩევ კომისიასთან. გასაუბრების წარმატებით გავლის შემთხვევაში ჩაირიცხებით კურსზე და გაფორმდება შესაბამისი ხელშეკრულება.",
+          stage4,
+        );
+
+        let questionAnswertext2 = document.createElement("p");
+        questionAnswertext2.textContent = `* სანამ კურსზე დარეგისტრირდები მნიშვნელოვანია, ყურადღებით წაიკითხო კურსის აღწერა, ნახო რას ისწავლი კურსის განმავლობაში და გაიგო გააჩნია თუ არა კურსს დასწრების წინაპირობა.`;
+        questionAnswer.appendChild(questionAnswertext2);
+      }
+
       e.target.id = "opened";
       answerOpen = true; // Set answer state to open
     } else {
@@ -39,7 +101,5 @@ function questionsAndAnswers(question, icon, answer) {
     }
   });
 }
-
-function clouseAnswer() {}
 
 export { questionsAndAnswers };
